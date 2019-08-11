41b09f2d6e399b6215c8f8c6a0732d270812677e
gsucarrat <31441084+gsucarrat@users.noreply.github.com>
2019-08-11 13:07:45 +0200

    New make-and-check file

--- gets-make-and-check-tarball.R
+++ gets-make-and-check-tarball.R
@@ -1,25 +1,33 @@
 ####################################################
 ## This file makes and checks the tarball of the 
-## gets package.
+## gets package, and installs it
 ##
 ## First created 2 July 2019 by Genaro Sucarrat
 ##
 ## Requirements:
 ##
 ## - Reading and writing permissions to the working
-##   directory that is used. On Windows: First
-##   (right-click mouse in folder, choose properties
-##    -> security ...etc.).
+##   directory that is used. To ensure this in
+##   Windows, first right-click mouse in folder,
+##   and then choose properties -> security ...etc..
 ##
-## - That the work directory contains the gets-devel
+## - That the work directory contains: the gets-devel
 ##   folder, and the gets-base-source.R and gets-
 ##   isat-source.R files
 ##
+## Contents:
+##
+## 1 INITIATE, CLEAN WORK DIRECTORY, LOAD SOURCE
+## 2 BUILD THE GETS FOLDER
+## 3 BUILD AND CHECK THE TARBALL
+## 4 INSTALL PACKAGE
+## 5 UPLOAD TO CRAN
+##
 ####################################################
 
 
 ####################################################
-## 1 INITIATE
+## 1 INITIATE, CLEAN WORK-DIRECTORY, LOAD SOURCE
 ####################################################
  
 ##Set working directory:
@@ -27,22 +35,23 @@ setwd(choose.dir())
 #Examples in Windows:
 #setwd("C:/Program files/R/R-devel/bin/")
 #setwd("C:/Program files/R/R-3.5.3/bin/")
+#setwd("C:/Users/sucarrat/Documents/R/gs/gets/devel/")
 
 ##delete files and folders from previous builds?
-deleteFiles <- FALSE #TRUE or FALSE?
+doDelete <- TRUE #TRUE or FALSE?
 
 ##clean:
-if(deleteFiles){
+if(doDelete){
 
-  ##files and folders of work-directory:
+  ##files and folders of the work-directory:
   fileNames <- dir()
   
   ##delete tarball, if it already exists:
   toBeDeleted <- fileNames[ grep(".tar.gz", fileNames) ] 
   if(length(toBeDeleted)>0){ file.remove(toBeDeleted) }
   
-  ##delete folders "gets", "gets-skeleton" and "gets.Rcheck"
-  ##if they exist:
+  ##delete folders "gets", "gets-skeleton" and "gets.Rcheck",
+  ##if they already exist:
   toBeDeleted <- intersect(c("gets","gets-skeleton","gets.Rcheck"),
     fileNames)
   if(length(toBeDeleted)>0){
@@ -51,18 +60,18 @@ if(deleteFiles){
     }
   }
 
-} #end if(deleteFiles)
+} #end if(doDelete)
 
 ##clean workspace:
 rm(list = ls())
 
-##Load sources:
+##Load source:
 source("gets-base-source.R")
 source("gets-isat-source.R")
 
 
 ####################################################
-## 2 ..
+## 2 BUILD THE GETS FOLDER
 ####################################################
 
 ##make skeleton (i.e. folder structure w/files):
@@ -71,33 +80,39 @@ package.skeleton(name="gets")
 ##rename skeleton:
 file.rename("gets", "gets-skeleton")
 
-##gets, gets-devel, gets-skeleton:
-##================================
-
-##copy files, make gets folder, etc.:
+##create new gets folder:
 dir.create("gets")
+
+##copy files from gets-devel to gets:
 fileNames <- dir("./gets-devel/")
 for(i in 1:length(fileNames)){
   file.copy( paste0("./gets-devel/", fileNames[i]), "gets", recursive=TRUE)
 }
 
+##copy files from skeleton to gets:
 fileNames <- dir("./gets-skeleton/R")
 fileNames <- setdiff(fileNames, "gets-internal.R") #remove gets-internal.R
 for(i in 1:length(fileNames)){
   file.copy( paste0("./gets-skeleton/R/", fileNames[i]), "./gets/R/")
 }
 
-## Note: Check that version is correct in gets-internal.R,
-## DESCRIPTION, start-up message and gets-package.RD
+## Remember to check that version and date are correct in:
+## - DESCRIPTION
+## - NEWS
+## - /R/gets-internal.R (start-up message)
+## - /man/gets-package.Rd
 
-##build and check tarball:
-##========================
+
+####################################################
+## 3 BUILD AND CHECK THE TARBALL
+####################################################
 
 ##build tarball:
 system("R CMD build gets --resave-data")
-## The --resave-data option is recommended for better compression.
-## However, it is not obligatory. In principle, the latest
-## development version of R should be used for the build.
+## - The --resave-data option is recommended for better compression.
+##   However, it is not obligatory.
+## - In principle, the latest development version of R should be
+##   used for the build.
 
 ##check tarball (needs internet):
 fileNames <- dir()
@@ -109,19 +124,23 @@ system( paste0("R CMD check ", tarballName, " --as-cran") )
 ## This is not detected by the tarball check. An alternative way to
 ## check tarball, see: http://win-builder.r-project.org/
 
-##If desirable, install package:
-##==============================
 
-##remove old version first?:
+####################################################
+## 4 INSTALL PACKAGE
+####################################################
+
+##remove old version first:
 remove.packages("gets")
 
-##install package:
+##install new version:
 system( paste0("R CMD INSTALL ", tarballName) )
 #system("R CMD INSTALL gets_0.20.tar.gz")
 #system("R CMD INSTALL --build gets")
 
-##Upload to CRAN:
-##===============
+
+####################################################
+## 5 UPLOAD TO CRAN
+####################################################
 
 ##When all the testing is done, upload the tarball, that is, the
 ##?.tar.gz? file, see "Writing R extensions" for the current
