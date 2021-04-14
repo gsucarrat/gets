f7ff2fddef98090a8db87ff9874136f2de1ab5b7
moritzpschwarz <moritz.schwarz@outlook.com>
2021-04-14 14:13:56 +0200

    udpated test and S3 code

--- gets/R/gets-lm-S3.R
+++ gets/R/gets-lm-S3.R
@@ -1,38 +1,82 @@
 
-isat.lm <- function(lmobject, ...){
+isat.lm <- function(lmobject, ar=NULL, ewma=NULL, iis=FALSE, sis=TRUE, tis=FALSE, uis=FALSE, blocks=NULL,
+                    ratio.threshold=0.8, max.block.size=30, t.pval=0.001,
+                    wald.pval=t.pval, vcov.type=c("ordinary", "white", "newey-west"),
+                    do.pet=FALSE, ar.LjungB=NULL, arch.LjungB=NULL,
+                    normality.JarqueB=NULL, info.method=c("sc", "aic", "hq"), 
+                    user.diagnostics=NULL, user.estimator=NULL, gof.function=NULL, 
+                    gof.method=c("min","max"), include.gum=NULL,
+                    include.1cut=FALSE, include.empty=FALSE, max.paths=NULL,
+                    parallel.options=NULL, turbo=FALSE, tol=1e-07, LAPACK=FALSE,
+                    max.regs=NULL, print.searchinfo=TRUE, plot=NULL, alarm=FALSE){
+  
+  # Checks
+  if(!is.null(lmobject$weights)){stop("Usage of weights is not yet implemented in isat. Please estimate the lm object without weights.")}
+  
   data <- model.frame(lmobject)
   y <- data.frame(y = data[,1])
   names(y) <- names(data)[1] 
   
   
   x_vars <- data[,2:ncol(data)]
-  mxreg <- model.matrix(lmobject$terms, data)
+  mxreg <- stats::model.matrix(lmobject$terms, data)
   # Deal with intercept
   mc <- ifelse(attr(lmobject$terms, "intercept")==1, TRUE, FALSE)
   mxreg <- mxreg[,!colnames(mxreg) == "(Intercept)"] # remove the intercept
   
-  out <- gets::isat(y = y, mxreg = mxreg, mc = mc, ...)
+  out <- gets::isat(y = y, mxreg = mxreg, mc = mc, 
+                    ar, ewma, iis, sis, tis, uis, blocks,
+                    ratio.threshold, max.block.size, t.pval,
+                    wald.pval, vcov.type,
+                    do.pet, ar.LjungB, arch.LjungB,
+                    normality.JarqueB, info.method, 
+                    user.diagnostics, user.estimator, gof.function, 
+                    gof.method, include.gum,
+                    include.1cut, include.empty, max.paths,
+                    parallel.options, turbo, tol, LAPACK,
+                    max.regs, print.searchinfo, plot, alarm)
   return(out)
 }
 
-getsm.lm <- function(lmobject, ...){
+getsm.lm <- function(lmobject, t.pval=0.05, wald.pval=t.pval, vcov.type=NULL,
+                     do.pet=TRUE, ar.LjungB=list(lag=NULL, pval=0.025),
+                     arch.LjungB=list(lag=NULL, pval=0.025), normality.JarqueB=NULL,
+                     user.diagnostics=NULL, info.method=c("sc","aic","aicc","hq"),
+                     gof.function=NULL, gof.method=NULL, keep=NULL, include.gum=FALSE,
+                     include.1cut=TRUE, include.empty=FALSE, max.paths=NULL, tol=1e-07,
+                     turbo=FALSE, print.searchinfo=TRUE, plot=NULL, alarm=FALSE){
   arx_object <- arx.lm(lmobject)
-  getsm(arx_object, ...)
+  getsm(arx_object, t.pval, wald.pval, vcov.type,do.pet, ar.LjungB,arch.LjungB, 
+        normality.JarqueB,user.diagnostics, info.method,gof.function, gof.method, keep, include.gum,
+        include.1cut, include.empty, max.paths, tol,turbo, print.searchinfo, plot, alarm)
 }
 
 
-arx.lm <- function(lmobject, ...){
+arx.lm <- function(lmobject, ar=NULL, ewma=NULL, mxreg=NULL,
+                   vc=FALSE, arch=NULL, asym=NULL, log.ewma=NULL, vxreg=NULL,
+                   zero.adj=0.1, vc.adj=TRUE,
+                   vcov.type=c("ordinary", "white", "newey-west"),
+                   qstat.options=NULL, normality.JarqueB=FALSE, user.estimator=NULL,
+                   user.diagnostics=NULL, tol=1e-07, LAPACK=FALSE, plot=NULL){
+  # Checks
+  if(!is.null(lmobject$weights)){stop("Usage of weights is not yet implemented in arx. Please estimate the lm object without weights.")}
+  
   data <- model.frame(lmobject)
   y <- data.frame(y = data[,1])
   names(y) <- names(data)[1] 
   
   
   x_vars <- data[,2:ncol(data)]
-  mxreg <- model.matrix(lmobject$terms, data)
+  mxreg <- stats::model.matrix(lmobject$terms, data)
   
   # Deal with intercept
   mc <- ifelse(attr(lmobject$terms, "intercept")==1, TRUE, FALSE)
   mxreg <- mxreg[,!colnames(mxreg) == "(Intercept)"] # remove the intercept
-  out <- arx(y = y, mxreg = mxreg, mc = mc, ...)
+  out <- arx(y = y, mxreg = mxreg, mc = mc, 
+             ar, ewma, vc, arch, asym, log.ewma, vxreg,
+             zero.adj, vc.adj,
+             vcov.type,
+             qstat.options, normality.JarqueB, user.estimator,
+             user.diagnostics, tol, LAPACK, plot)
   return(out)
 }
--- gets/tests/testthat/test-S3-lm.R
+++ gets/tests/testthat/test-S3-lm.R
@@ -8,16 +8,6 @@
 #
 # @examples
 # Simplest use
-model1 <- lm(mpg~ cyl  + gear + carb + hp + drat, mtcars)
-isat(model1)
-
-# With further options
-isat(model1, t.pval = 0.01, iis = TRUE, vcov.type = "white")
-
-# More complicated model
-model2 <- lm(log(mpg)~ cyl  + as.factor(gear) + carb + hp + I(hp*hp) + drat + I(drat*carb), mtcars)
-isat(model2)
-
 
 # General-to-Specific (GETS) Modelling for an `lm` object (S3 Method)
 #
@@ -29,15 +19,7 @@ isat(model2)
 #
 # @examples
 # Simplest use
-model1 <- lm(mpg~ cyl  + gear + carb + hp + drat, mtcars)
-getsm(model1)
 
-# With further options
-getsm(model1, vcov.type = "white")
-
-# More complicated model
-model2 <- lm(log(mpg)~ cyl  + as.factor(gear) + carb + hp + I(hp*hp) + drat + I(drat*carb), mtcars)
-getsm(model2)
 
 # Estimate an AR-X model with log-ARCH-X errors (S3 Method for lm)
 #
@@ -49,14 +31,245 @@ getsm(model2)
 #
 # @examples
 # # Simplest use
-model1 <- lm(mpg~ cyl  + gear + carb + hp + drat, mtcars)
-arx(model1)
+mtcars
 
-# With further options
-arx(model1, t.pval = 0.01, vcov.type = "white")
+# arx testing -------------------------------------------------------------
 
-# More complicated model
-model2 <- lm(log(mpg)~ cyl  + as.factor(gear) + carb + hp + I(hp*hp) + drat + I(drat*carb), mtcars)
-arx(model2)
+test_that("Check coefficients are equal between lm and arx",{
+
+  # A number of models with varying formula arguments
+  lm_model1 <- lm(mpg~ cyl  + gear + carb + hp + drat, mtcars)
+  lm_model2 <- lm(log(mpg)~ cyl  + as.factor(gear) + carb + hp + I(hp*hp) + drat + I(drat*carb), mtcars)
+  lm_model3 <- lm(cyl ~ log(mpg) + as.factor(gear) + log(carb) + hp + I(hp*hp) + lag(drat) + I(drat*carb) + mpg*carb + hp:drat, mtcars)
+  lm_model4 <- lm(cyl ~ log(mpg) + as.factor(gear) + log(carb) + hp + I(hp*hp) + lag(drat) + I(drat*carb) + mpg*carb + hp:drat-1, mtcars)
+  
+  arx_model1 <- arx.lm(lm_model1)
+  arx_model2 <- arx.lm(lm_model2)
+  arx_model3 <- arx.lm(lm_model3)
+  arx_model4 <- arx.lm(lm_model4)
+  
+  coef_lm_model1 <- coef(lm_model1)
+  coef_lm_model2 <- coef(lm_model2)
+  coef_lm_model3 <- coef(lm_model3)
+  coef_lm_model4 <- coef(lm_model4)
+  
+  coef_arx_model1 <- coef(arx_model1)
+  coef_arx_model2 <- coef(arx_model2)
+  coef_arx_model3 <- coef(arx_model3)
+  coef_arx_model4 <- coef(arx_model4)
+  
+  # modify the intercept name
+  names(coef_lm_model1)[which(names(coef_lm_model1)=="(Intercept)")] <- "mconst"
+  names(coef_lm_model2)[which(names(coef_lm_model2)=="(Intercept)")] <- "mconst"
+  names(coef_lm_model3)[which(names(coef_lm_model3)=="(Intercept)")] <- "mconst"
+  names(coef_lm_model4)[which(names(coef_lm_model4)=="(Intercept)")] <- "mconst"
+  
+  
+  expect_equal(coef_lm_model1, coef_arx_model1)
+  expect_equal(coef_lm_model2, coef_arx_model2)
+  expect_equal(coef_lm_model3, coef_arx_model3)
+  expect_equal(coef_lm_model4, coef_arx_model4)
+  
+  
+})
+
+
+test_that("Check that the coefficients are equal between lm and arx but with vcov.type the S.E. are different",{
+  
+  # A number of models with varying formula arguments
+  lm_model1 <- lm(mpg~ cyl  + gear + carb + hp + drat, mtcars)
+  lm_model2 <- lm(log(mpg)~ cyl  + as.factor(gear) + carb + hp + I(hp*hp) + drat + I(drat*carb), mtcars)
+  lm_model3 <- lm(cyl ~ log(mpg) + as.factor(gear) + log(carb) + hp + I(hp*hp) + lag(drat) + I(drat*carb) + mpg*carb + hp:drat, mtcars)
+  lm_model4 <- lm(cyl ~ log(mpg) + as.factor(gear) + log(carb) + hp + I(hp*hp) + lag(drat) + I(drat*carb) + mpg*carb + hp:drat-1, mtcars)
+  
+  arx_model1 <- arx.lm(lm_model1, vcov.type = "white")
+  arx_model2 <- arx.lm(lm_model2, vcov.type = "white")
+  arx_model3 <- arx.lm(lm_model3, vcov.type = "newey-west")
+  arx_model4 <- arx.lm(lm_model4, vcov.type = "newey-west")
+  
+  coef_lm_model1 <- coef(lm_model1)
+  coef_lm_model2 <- coef(lm_model2)
+  coef_lm_model3 <- coef(lm_model3)
+  coef_lm_model4 <- coef(lm_model4)
+  
+  coef_arx_model1 <- coef(arx_model1)
+  coef_arx_model2 <- coef(arx_model2)
+  coef_arx_model3 <- coef(arx_model3)
+  coef_arx_model4 <- coef(arx_model4)
+  
+  # modify the intercept name
+  names(coef_lm_model1)[which(names(coef_lm_model1)=="(Intercept)")] <- "mconst"
+  names(coef_lm_model2)[which(names(coef_lm_model2)=="(Intercept)")] <- "mconst"
+  names(coef_lm_model3)[which(names(coef_lm_model3)=="(Intercept)")] <- "mconst"
+  names(coef_lm_model4)[which(names(coef_lm_model4)=="(Intercept)")] <- "mconst"
+  
+  
+  expect_equal(coef_lm_model1, coef_arx_model1)
+  expect_equal(coef_lm_model2, coef_arx_model2)
+  expect_equal(coef_lm_model3, coef_arx_model3)
+  expect_equal(coef_lm_model4, coef_arx_model4)
+  
+  expect_false(all(vcov(lm_model1)==vcov(arx_model1)))
+  expect_false(all(vcov(lm_model2)==vcov(arx_model2)))
+  expect_false(all(vcov(lm_model3)==vcov(arx_model3)))
+  expect_false(all(vcov(lm_model4)==vcov(arx_model4)))
+
+})
+
+
+
+# getsm testing -----------------------------------------------------------
+
+test_that("Test that getsm works with lm models",{
+  
+  # A number of models with varying formula arguments
+  lm_model1 <- lm(mpg~ cyl  + gear + carb + hp + drat, mtcars)
+  lm_model2 <- lm(log(mpg)~ cyl  + as.factor(gear) + carb + hp + I(hp*hp) + drat + I(drat*carb), mtcars)
+  lm_model3 <- lm(cyl ~ log(mpg) + as.factor(gear) + log(carb) + hp + I(hp*hp) + lag(drat) + I(drat*carb) + mpg*carb + hp:drat, mtcars)
+  lm_model4 <- lm(cyl ~ log(mpg) + as.factor(gear) + log(carb) + hp + I(hp*hp) + lag(drat) + I(drat*carb) + mpg*carb + hp:drat-1, mtcars)
+  
+  
+  getsm_model1 <- getsm.lm(lm_model1, print.searchinfo = FALSE)
+  getsm_model2 <- getsm.lm(lm_model2, print.searchinfo = FALSE)
+  getsm_model3 <- getsm.lm(lm_model3, print.searchinfo = FALSE)
+  getsm_model4 <- getsm.lm(lm_model4, print.searchinfo = FALSE)
+  
+  expect_output(print(getsm_model1))
+  expect_output(print(getsm_model2))
+  expect_output(print(getsm_model3))
+  expect_output(print(getsm_model4))
+  
+  # Check that all variable names in getsm are also in the lm object
+  coef_lm_model1 <- coef(lm_model1)
+  coef_lm_model2 <- coef(lm_model2)
+  coef_lm_model3 <- coef(lm_model3)
+  coef_lm_model4 <- coef(lm_model4)
+  
+  # modify the intercept name
+  names(coef_lm_model1)[which(names(coef_lm_model1)=="(Intercept)")] <- "mconst"
+  names(coef_lm_model2)[which(names(coef_lm_model2)=="(Intercept)")] <- "mconst"
+  names(coef_lm_model3)[which(names(coef_lm_model3)=="(Intercept)")] <- "mconst"
+  names(coef_lm_model4)[which(names(coef_lm_model4)=="(Intercept)")] <- "mconst"
+  
+  expect_true(all(names(coef(getsm_model1)) %in% names(coef_lm_model1)))
+  expect_true(all(names(coef(getsm_model2)) %in% names(coef_lm_model2)))
+  expect_true(all(names(coef(getsm_model3)) %in% names(coef_lm_model3)))
+  expect_true(all(names(coef(getsm_model4)) %in% names(coef_lm_model4)))
+  
+  # Check that the number of variables is always smaller than the original model
+  expect_true(length(coef(getsm_model1)) <= length(coef(lm_model1)))
+  expect_true(length(coef(getsm_model2)) <= length(coef(lm_model2)))
+  expect_true(length(coef(getsm_model3)) <= length(coef(lm_model3)))
+  expect_true(length(coef(getsm_model4)) <= length(coef(lm_model4)))
+  
+})
+
+
+# isat testing ------------------------------------------------------------
+
+
+test_that("Test that isat works with lm models",{
+  
+  lm_model1 <- lm(GD ~ FMBASE + FSDJ + LHC + MU, gets::hpdata)
+  lm_model2 <- lm(log(GD) ~ log(FMBASE) + FSDJ + LHC + MU, gets::hpdata)
+  lm_model3 <- lm(log(GD) ~ log(FMBASE) + FSDJ*LHC + MU, gets::hpdata)
+  lm_model4 <- lm(GD ~ FMBASE + FSDJ + LHC + MU + I(MU*MU)-1, gets::hpdata)
+  
+  # A number of models with varying formula arguments
+  # lm_model1 <- lm(mpg~ cyl  + gear + carb + hp + drat, mtcars)
+  # lm_model2 <- lm(log(mpg)~ cyl  + as.factor(gear) + carb + hp + I(hp*hp) + drat + I(drat*carb), mtcars)
+  # lm_model3 <- lm(cyl ~ log(mpg) + as.factor(gear) + log(carb) + hp + I(hp*hp) + lag(drat) + I(drat*carb) + mpg*carb + hp:drat, mtcars)
+  # lm_model4 <- lm(cyl ~ log(mpg) + as.factor(gear) + log(carb) + hp + I(hp*hp) + lag(drat) + I(drat*carb) + mpg*carb + hp:drat-1, mtcars)
+  # 
+  # 
+  isat_model1 <- isat.lm(lm_model1, print.searchinfo = FALSE, t.pval = 0.001)
+  isat_model2 <- isat.lm(lm_model2, print.searchinfo = FALSE, t.pval = 0.001)
+  isat_model3 <- isat.lm(lm_model3, print.searchinfo = FALSE, t.pval = 0.001)
+  isat_model4 <- isat.lm(lm_model4, print.searchinfo = FALSE, t.pval = 0.001, ar = 1) # adding ar term
+  
+  expect_output(print(isat_model1))
+  expect_output(print(isat_model2))
+  expect_output(print(isat_model3))
+  expect_output(print(isat_model4))
+  
+  # Check that all variable names in isat are also in the lm object
+  coef_lm_model1 <- coef(lm_model1)
+  coef_lm_model2 <- coef(lm_model2)
+  coef_lm_model3 <- coef(lm_model3)
+  coef_lm_model4 <- coef(lm_model4)
+  
+  # modify the intercept name
+  names(coef_lm_model1)[which(names(coef_lm_model1)=="(Intercept)")] <- "mconst"
+  names(coef_lm_model2)[which(names(coef_lm_model2)=="(Intercept)")] <- "mconst"
+  names(coef_lm_model3)[which(names(coef_lm_model3)=="(Intercept)")] <- "mconst"
+  names(coef_lm_model4)[which(names(coef_lm_model4)=="(Intercept)")] <- "mconst"
+  
+  
+  # Check that the number of variables is always equal or larger than the original model
+  expect_true(length(coef(isat_model1)) >= length(coef(lm_model1)))
+  expect_true(length(coef(isat_model2)) >= length(coef(lm_model2)))
+  expect_true(length(coef(isat_model3)) >= length(coef(lm_model3)))
+  expect_true(length(coef(isat_model4)) >= length(coef(lm_model4)))
+  
+})
+
+# Special case testing ----------------------------------------------------
+
+
+test_that("Check that the error message displays when weights are used in lm",{
+  lm_model5 <- lm(mpg~ cyl + hp, weights = rnorm(nrow(mtcars),mean = 100), mtcars)
+  expect_error(arx.lm(lm_model5),regexp = "Usage of weights is not yet implemented in arx. Please estimate the lm object without weights.")
+  
+})
+
+# 
+# 
+# For the examples
+# 
+# 
+# # More complicated model
+# model2 <- lm(log(mpg)~ cyl  + as.factor(gear) + carb + hp + I(hp*hp) + drat + I(drat*carb), mtcars)
+# arx(model2)
+# 
+# model3 <- lm(log(mpg)~ cyl  + as.factor(gear) + log(carb) + hp + I(hp*hp) + lag(drat) + I(drat*carb) + cyl*carb + hp:drat, mtcars)
+# summary(model3)
+# arx.lm(model3)
+# 
+# getsm.lm(model3)
+# 
+# isat.lm(model3)
+# 
+# 
+# 
+# 
+# 
+# 
+# model1 <- lm(mpg~ cyl  + gear + carb + hp + drat, mtcars)
+# isat.model1 <- isat.lm(model1)
+# 
+# varnames_lm <- names(model1$coefficients)
+# varnames_lm <- setdiff(varnames_lm,"(Intercept)")
+# names(coef(isat.model1))
+# 
+# # With further options
+# isat(model1, t.pval = 0.01, iis = TRUE, vcov.type = "white")
+# 
+# # More complicated model
+# model2 <- lm(log(mpg)~ cyl  + as.factor(gear) + carb + hp + I(hp*hp) + drat + I(drat*carb), mtcars)
+# isat(model2)
+# 
+# 
+# 
+# model1 <- lm(mpg~ cyl  + gear + carb + hp + drat, mtcars)
+# getsm(model1)
+# 
+# # With further options
+# getsm(model1, vcov.type = "white")
+# 
+# # More complicated model
+# model2 <- lm(log(mpg)~ cyl  + as.factor(gear) + carb + hp + I(hp*hp) + drat + I(drat*carb), mtcars)
+# getsm(model2)
+# 
+
 
 
