8fcf95caf2287d0c1d77e987adf9e0b26808246c
moritzpschwarz <moritz.schwarz@outlook.com>
2021-04-19 17:08:33 +0200

    changes to S3 methods (mainly to do with the call aspect)

--- gets/R/gets-lm-S3.R
+++ gets/R/gets-lm-S3.R
@@ -85,56 +85,59 @@ isat.arx <- function(
   # if not, then check if the original item has this arguemnt supplied
   # if it does, take the setting of the original object
   # if it does not, then take the default
-  if(missing(ar)){ar <- if(is.null(y$call[["ar"]])) {NULL} else{y$call[["ar"]]}}
+  if(missing(ar)){ar <- if(is.null(y$aux$arguments[["ar"]])) {NULL} else{y$aux$arguments[["ar"]]}}
   if(missing(vcov.type)){vcov.type <- y$aux[["vcov.type"]]}
-  if(missing(normality.JarqueB)){normality.JarqueB <- if(is.null(y$call[["normality.JarqueB"]])){FALSE}else{y$call[["normality.JarqueB"]]}}
-  if(missing(user.estimator)){user.estimator <- if(is.null(y$call[["user.estimator"]])){NULL}else{y$call[["user.estimator"]]}}
-  if(missing(user.diagnostics)){user.diagnostics <- if(is.null(y$call[["user.diagnostics"]])){NULL}else{y$call[["user.diagnostics"]]}}
-  if(missing(LAPACK)){LAPACK <- if(is.null(y$call[["LAPACK"]])){FALSE}else{y$call[["LAPACK"]]}}
-  if(missing(plot)){plot <- if(is.null(y$call[["plot"]])){NULL}else{y$call[["plot"]]}}
-  if(missing(tol)){tol <- if(is.null(y$call[["tol"]])){1e-07}else{y$call[["tol"]]}}
+  if(missing(normality.JarqueB)){normality.JarqueB <- if(is.null(y$aux$arguments[["normality.JarqueB"]])){FALSE}else{y$aux$arguments[["normality.JarqueB"]]}}
+  if(missing(user.estimator)){user.estimator <- if(is.null(y$aux$arguments[["user.estimator"]])){NULL}else{y$aux$arguments[["user.estimator"]]}}
+  if(missing(user.diagnostics)){user.diagnostics <- if(is.null(y$aux$arguments[["user.diagnostics"]])){NULL}else{y$aux$arguments[["user.diagnostics"]]}}
+  if(missing(LAPACK)){LAPACK <- if(is.null(y$aux$arguments[["LAPACK"]])){FALSE}else{y$aux$arguments[["LAPACK"]]}}
+  if(missing(plot)){plot <- if(is.null(y$aux$arguments[["plot"]])){NULL}else{y$aux$arguments[["plot"]]}}
+  if(missing(tol)){tol <- if(is.null(y$aux$arguments[["tol"]])){1e-07}else{y$aux$arguments[["tol"]]}}
+  
+  mxreg <- y$aux$mX
+  colnames(mxreg) <- y$aux$mXnames
   
   out <- isat.default(
-    y = y$aux$y,
-    mxreg = y$aux$mX,
-    mc = FALSE,
-    ar = ar,
-    ewma = ewma,
-    iis = iis,
-    sis = sis,
-    tis = tis,
-    uis = uis,
-    blocks = blocks,
-    ratio.threshold = ratio.threshold,
-    max.block.size = max.block.size,
-    t.pval = t.pval,
-    wald.pval = wald.pval,
-    vcov.type = vcov.type,
-    do.pet = do.pet,
-    ar.LjungB = ar.LjungB,
-    arch.LjungB = arch.LjungB,
-    normality.JarqueB = normality.JarqueB,
-    info.method = info.method,
-    user.diagnostics = user.diagnostics,
-    user.estimator = user.estimator,
-    gof.function = gof.function,
-    gof.method = gof.method,
-    include.gum = include.gum,
-    include.1cut = include.1cut,
-    include.empty = include.empty,
-    max.paths = max.paths,
-    parallel.options = parallel.options,
-    turbo = turbo,
-    tol = tol,
-    LAPACK = LAPACK,
-    max.regs = max.regs,
-    print.searchinfo = print.searchinfo,
-    plot = FALSE,
-    alarm = alarm
+    y$aux$y,
+    FALSE,
+    ar,
+    ewma,
+    mxreg,
+    iis,
+    sis,
+    tis,
+    uis,
+    blocks,
+    ratio.threshold,
+    max.block.size,
+    t.pval,
+    wald.pval,
+    vcov.type,
+    do.pet,
+    ar.LjungB,
+    arch.LjungB,
+    normality.JarqueB,
+    info.method,
+    user.diagnostics,
+    user.estimator,
+    gof.function,
+    gof.method,
+    include.gum,
+    include.1cut,
+    include.empty,
+    max.paths,
+    parallel.options,
+    turbo,
+    tol,
+    LAPACK,
+    max.regs,
+    print.searchinfo,
+    plot,
+    alarm
   )
-  if(plot){
-    plot(out)
-  }
+  # if(plot){
+  #   plot(out)
+  # }
   
   return(out)
 }
@@ -152,30 +155,46 @@ arx.isat <- function(y, ar=NULL, ewma=NULL,
   # if not, then check if the original item has this arguemnt supplied
   # if it does, take the setting of the original object
   # if it does not, then take the default
-  if(missing(ar)){ar <- if(is.null(y$call[["ar"]])) {NULL} else{y$call[["ar"]]}}
+  if(missing(ar)){ar <- if(is.null(y$aux$arguments[["ar"]])) {NULL} else{y$aux$arguments[["ar"]]}}
   if(missing(vcov.type)){vcov.type <- y$aux[["vcov.type"]]}
-  if(missing(normality.JarqueB)){normality.JarqueB <- if(is.null(y$call[["normality.JarqueB"]])){FALSE}else{y$call[["normality.JarqueB"]]}}
-  if(missing(user.estimator)){user.estimator <- if(is.null(y$call[["user.estimator"]])){NULL}else{y$call[["user.estimator"]]}}
-  if(missing(user.diagnostics)){user.diagnostics <- if(is.null(y$call[["user.diagnostics"]])){NULL}else{y$call[["user.diagnostics"]]}}
-  if(missing(LAPACK)){LAPACK <- if(is.null(y$call[["LAPACK"]])){FALSE}else{y$call[["LAPACK"]]}}
-  if(missing(plot)){plot <- if(is.null(y$call[["plot"]])){NULL}else{y$call[["plot"]]}}
-  if(missing(tol)){tol <- if(is.null(y$call[["tol"]])){1e-07}else{y$call[["tol"]]}}
+  if(missing(normality.JarqueB)){normality.JarqueB <- if(is.null(y$aux$arguments[["normality.JarqueB"]])){FALSE}else{y$aux$arguments[["normality.JarqueB"]]}}
+  if(missing(user.estimator)){user.estimator <- if(is.null(y$aux$arguments[["user.estimator"]])){NULL}else{y$aux$arguments[["user.estimator"]]}}
+  if(missing(user.diagnostics)){user.diagnostics <- if(is.null(y$aux$arguments[["user.diagnostics"]])){NULL}else{y$aux$arguments[["user.diagnostics"]]}}
+  if(missing(LAPACK)){LAPACK <- if(is.null(y$aux$arguments[["LAPACK"]])){FALSE}else{y$aux$arguments[["LAPACK"]]}}
+  if(missing(plot)){plot <- if(is.null(y$aux$arguments[["plot"]])){NULL}else{y$aux$arguments[["plot"]]}}
+  if(missing(tol)){tol <- if(is.null(y$aux$arguments[["tol"]])){1e-07}else{y$aux$arguments[["tol"]]}}
   
-  
-  dep_var <- data.frame(y$aux$y)
-  names(dep_var) <- y$aux$y.name 
+  browser()
+  dep_var <- matrix(y$aux$y)
+  colnames(dep_var) <- y$aux$y.name 
   
   mxreg <- y$aux$mX
   
-  out <- arx(y = dep_var, mxreg = mxreg, mc = FALSE, 
-             ar = ar, ewma = ewma,
-             vc = vc, arch = arch, asym = asym, log.ewma = log.ewma, vxreg = vxreg,
-             zero.adj = zero.adj, vc.adj = vc.adj,
-             vcov.type = vcov.type,
-             qstat.options = qstat.options, normality.JarqueB = normality.JarqueB, user.estimator = user.estimator,
-           user.diagnostics = user.diagnostics, tol = tol, LAPACK = LAPACK, plot = FALSE)
-  if(plot){
-    plot(arx)
+  out <- arx(
+    dep_var,
+    FALSE,
+    ar,
+    ewma,
+    mxreg = mxreg,
+    vc,
+    arch,
+    asym,
+    log.ewma,
+    vxreg,
+    zero.adj,
+    vc.adj,
+    vcov.type,
+    qstat.options,
+    normality.JarqueB,
+    user.estimator = user.estimator,
+    user.diagnostics,
+    tol,
+    LAPACK,
+    FALSE
+  )
+  
+  if(!is.null(plot) && plot){
+    plot(out)
   }
   
   return(out)
@@ -192,19 +211,44 @@ gets.isat <- function(x, t.pval=0.05, wald.pval=t.pval, vcov.type = NULL,
                       turbo=FALSE, print.searchinfo=TRUE, plot=NULL, alarm=FALSE, ...)
 {
   
-  if(missing(vcov.type)){vcov.type <- x$aux$vcov.type}
-  if(missing(info.method)){info.method <- "sc"}
+  # Check if one of these arguments is explicitly supplied to the function
+  # if not, then check if the original item has this argument supplied
+  # if it does, take the setting of the original object
+  # if it does not, then take the default
+  if(missing(t.pval)){t.pval <- if(is.null(x$aux$arguments[["t.pval"]])) {0.05} else{x$aux$arguments[["t.pval"]]}}
+  if(missing(vcov.type)){vcov.type <- x$aux[["vcov.type"]]}
+  if(missing(normality.JarqueB)){normality.JarqueB <- if(is.null(x$aux$arguments[["normality.JarqueB"]])){FALSE}else{x$aux$arguments[["normality.JarqueB"]]}}
+  if(missing(user.diagnostics)){user.diagnostics <- if(is.null(x$aux$arguments[["user.diagnostics"]])){NULL}else{x$aux$arguments[["user.diagnostics"]]}}
+  if(missing(plot)){plot <- if(is.null(x$aux$arguments[["plot"]])){NULL}else{x$aux$arguments[["plot"]]}}
+  if(missing(tol)){tol <- if(is.null(x$aux$arguments[["tol"]])){1e-07}else{x$aux$arguments[["tol"]]}}
   
-  object <- arx(x, vcov.type = vcov.type)
- 
-  out <- getsm(object,
-               t.pval = t.pval, wald.pval = wald.pval, vcov.type = vcov.type,do.pet = do.pet, 
-               ar.LjungB = ar.LjungB,arch.LjungB = arch.LjungB,normality.JarqueB = normality.JarqueB,
-               user.diagnostics = user.diagnostics, info.method = info.method,
-               gof.function = gof.function, gof.method = gof.method, keep = keep, 
-               include.1cut= include.1cut, include.empty = include.empty, 
-               max.paths = max.paths, tol = tol,turbo = turbo, print.searchinfo = print.searchinfo, 
-               plot = plot, alarm = alarm)
+  
+  object <- arx(x, plot = FALSE)
+
+  out <- getsm(
+    object,
+    t.pval,
+    wald.pval,
+    vcov.type,
+    do.pet,
+    ar.LjungB,
+    arch.LjungB,
+    normality.JarqueB,
+    user.diagnostics,
+    info.method,
+    gof.function,
+    gof.method,
+    keep,
+    include.gum,
+    include.1cut,
+    include.empty,
+    max.paths,
+    tol,
+    turbo,
+    print.searchinfo,
+    plot,
+    alarm
+  )
   return(out)
 }
 
@@ -215,43 +259,45 @@ arx.lm <- function(y, ar=NULL, ewma=NULL,
                    vcov.type=c("ordinary", "white", "newey-west"),
                    qstat.options=NULL, normality.JarqueB=FALSE, user.estimator=NULL,
                    user.diagnostics=NULL, tol=1e-07, LAPACK=FALSE, plot=NULL, ...){
-  
   # Checks
   if(!is.null(y$weights)){stop("Usage of weights is not yet implemented in arx. Please estimate the lm y without weights.")}
   
   data <- stats::model.frame(y)
-  dep_var <- data.frame(y = data[,1])
-  names(dep_var) <- names(data)[1] 
+  dep_var <- matrix(data[,1])
+  colnames(dep_var) <- names(data)[1] 
   
   
   x_vars <- data[,2:ncol(data)]
+  
   mxreg <- stats::model.matrix(y$terms, data)
+  invalid_variables_in_lm <- names(y$coef)[is.na(y$coef)]
+  mxreg <- mxreg[,!colnames(mxreg) %in% invalid_variables_in_lm]
   
   # Deal with intercept
   mc <- ifelse(attr(y$terms, "intercept")==1, TRUE, FALSE)
   mxreg <- mxreg[,!colnames(mxreg) == "(Intercept)"] # remove the intercept
   
   out <- arx.default(
-    y = dep_var,
-    mxreg = mxreg,
-    mc = mc,
-    ar = ar,
-    ewma = ewma,
-    vc = vc,
-    arch = arch,
-    asym = asym,
-    log.ewma = log.ewma,
-    vxreg = vxreg,
-    zero.adj = zero.adj,
-    vc.adj = vc.adj,
-    vcov.type = vcov.type,
-    qstat.options = qstat.options,
-    normality.JarqueB = normality.JarqueB,
-    user.estimator =  user.estimator,
-    user.diagnostics = user.diagnostics,
-    tol = tol,
-    LAPACK = LAPACK,
-    plot =  plot
+    dep_var,
+    mc,
+    ar,
+    ewma,
+    mxreg,
+    vc,
+    arch,
+    asym,
+    log.ewma,
+    vxreg,
+    zero.adj,
+    vc.adj,
+    vcov.type,
+    qstat.options,
+    normality.JarqueB,
+    user.estimator,
+    user.diagnostics,
+    tol,
+    LAPACK,
+    plot
   )
   return(out)
 }
@@ -267,12 +313,32 @@ gets.lm <- function(x, t.pval=0.05, wald.pval=t.pval, vcov.type=NULL,
                     include.1cut=TRUE, include.empty=FALSE, max.paths=NULL, tol=1e-07,
                     turbo=FALSE, print.searchinfo=TRUE, plot=NULL, alarm=FALSE, ...)
 {
-  arx_object <- arx(x)
-  out <- getsm(object = arx_object, t.pval = t.pval, wald.pval = wald.pval,vcov.type =  vcov.type,do.pet = do.pet, ar.LjungB = ar.LjungB,
-               arch.LjungB = arch.LjungB,normality.JarqueB = normality.JarqueB,user.diagnostics = user.diagnostics, info.method = info.method,
-               gof.function = gof.function, gof.method = gof.method, keep = keep, include.gum = include.gum,
-               include.1cut = include.1cut, include.empty = include.empty, max.paths = max.paths, tol = tol,
-               turbo = turbo, print.searchinfo = print.searchinfo, plot = plot, alarm = alarm)
+  arx_object <- arx.lm(x)
+  
+  out <- getsm(
+    arx_object,
+    t.pval,
+    wald.pval,
+    vcov.type,
+    do.pet,
+    ar.LjungB,
+    arch.LjungB,
+    normality.JarqueB,
+    user.diagnostics,
+    info.method,
+    gof.function,
+    gof.method,
+    keep,
+    include.gum,
+    include.1cut,
+    include.empty,
+    max.paths,
+    tol,
+    turbo,
+    print.searchinfo,
+    plot,
+    alarm
+  )
   return(out)
 }
 
