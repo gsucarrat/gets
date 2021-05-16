21aeafcf01b83da303376b744ddf99ae3ddcc098
Jonas Kurle <56522916+jkurle@users.noreply.github.com>
2021-05-16 09:46:31 +0100

    keep colnames of regressors in arx

--- gets/R/gets-base-source.R
+++ gets/R/gets-base-source.R
@@ -2203,7 +2203,7 @@ arx <- function(y, mc=FALSE, ar=NULL, ewma=NULL, mxreg=NULL,
   if( NCOL(tmp)>1 ){
     aux$mX <- cbind(coredata(tmp[,-1]))
     aux$mXnames <- colnames(tmp)[-1]
-    colnames(aux$mX) <- NULL
+    #colnames(aux$mX) <- NULL
     aux$mXncol <- NCOL(aux$mX)
   }
 
@@ -2392,7 +2392,6 @@ arx <- function(y, mc=FALSE, ar=NULL, ewma=NULL, mxreg=NULL,
     p.val <- pt(abs(t.stat), out$df, lower.tail=FALSE)*2
     out$mean.results <- as.data.frame(cbind(out$coefficients,
       stderrs, t.stat, p.val))
-    print(out$mean.results)
     colnames(out$mean.results) <- c("coef", "std.error",
       "t-stat", "p-value")
     rownames(out$mean.results) <- aux$mXnames
