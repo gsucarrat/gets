4bc15d791512c6dcd4ecb1684e5971add4f3259b
gsucarrat <31441084+gsucarrat@users.noreply.github.com>
2022-03-22 10:42:25 +0100

    Update

--- gets/R/gets-internal.R
+++ gets/R/gets-internal.R
@@ -2,7 +2,7 @@
 {
   ##set start-up message:
   txt <- c("\n",
-    paste(sQuote("gets"), "version 0.33\n"),
+    paste(sQuote("gets"), "version 0.34\n"),
     "\n",
     paste("General-to-Specific (GETS) and Indicator Saturation (ISAT) methods"),
     "\n",
