339d0e9c1b5321b326aaa4a9211cf22e7a0f45ec
moritzpschwarz <moritz.schwarz@outlook.com>
2022-08-28 01:13:30 +0200

    dealing with ar == NULL

--- gets/R/gets-isat-source.R
+++ gets/R/gets-isat-source.R
@@ -58,7 +58,7 @@ isat.default <- function(y, mc=TRUE, ar=NULL, ewma=NULL, mxreg=NULL,
     stop("No Indicator Selection Method was selected. Either set iis, sis or tis as TRUE or specify uis.")
   }
   
-  if(ar == 0){ar <- NULL}
+  if(!is.null(ar) && ar == 0){ar <- NULL}
   if(!(is.numeric(ar) | is.null(ar))){stop("The 'ar' argument must be NULL or numeric.")}
   
   ##name of regressand:
